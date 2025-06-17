419e0460410a2a8e69d4b1c586c1a5c7d7b23b55
codewithbhandu <bhandariprashant230@gmail.com>
2025-06-17 13:14:13 +0545

    feat:

--- lib/logic/loginbloc/login_bloc.dart
+++ lib/logic/loginbloc/login_bloc.dart
@@ -18,6 +18,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     if (result) {
      emit(LoginSuccessful());
     } else {
+      //byfgygutfydrtusrjdfghjbjh
      emit(LoginFailure());
     }
 
